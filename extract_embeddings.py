28fcd6f3a2f022eb2c42b84861172af45b1f44d9
surajnair666 <48486555+surajnair666@users.noreply.github.com>
2020-09-10 11:01:47 +0530

    Add files via upload

---
+++ extract_embeddings.py
@@ -0,0 +1,73 @@
+
+from imutils import paths
+import numpy as np
+import imutils
+import pickle
+import cv2
+import os
+
+# load face detector
+print("[INFO] loading face detector...")
+protoPath = os.path.abspath("face_detection_model/deploy.prototxt")
+modelPath = os.path.abspath("face_detection_model/res10_300x300_ssd_iter_140000.caffemodel")
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# load face embedding model
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch("openface_nn4.small2.v1.t7")
+
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images("dataset")) #imutils function
+
+knownEmbeddings = []
+knownNames = []
+total = 0
+
+for (i, imagePath) in enumerate(imagePaths):
+	print("[INFO] processing image {}/{}".format(i + 1,
+		len(imagePaths)))
+	name = imagePath.split(os.path.sep)[1]
+
+	image = cv2.imread(imagePath)
+	image = imutils.resize(image, width=600)
+	(h, w) = image.shape[:2]
+
+	# construct a blob from the image
+	imageBlob = cv2.dnn.blobFromImage(
+		cv2.resize(image, (300, 300)), 1.0, (300, 300),
+		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+	detector.setInput(imageBlob)
+	detections = detector.forward()
+
+	if len(detections) > 0:
+		i = np.argmax(detections[0, 0, :, 2])
+		confidence = detections[0, 0, i, 2]
+
+		if confidence > 0.5:
+		
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			#  ROI
+			face = image[startY:endY, startX:endX]
+			(fH, fW) = face.shape[:2]
+
+			if fW < 20 or fH < 20:
+				continue
+
+			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+				(96, 96), (0, 0, 0), swapRB=True, crop=False)
+			embedder.setInput(faceBlob)
+			vec = embedder.forward()
+
+			knownNames.append(name)
+			knownEmbeddings.append(vec.flatten())
+			total += 1
+
+# Saving to disk
+print("[INFO] serializing {} encodings...".format(total))
+data = {"embeddings": knownEmbeddings, "names": knownNames}
+f = open("output/embeddings.pickle", "wb")
+f.write(pickle.dumps(data))
+f.close()
